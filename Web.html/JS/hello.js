6d493998952fabb6e940203a85f6280e7de5fdbb
1giay1nam1 <94155201+1giay1nam1@users.noreply.github.com>
2023-06-19 16:48:37 +0700

    commit

---
+++ Web.html/JS/hello.js
@@ -0,0 +1,9 @@
+function run(a, b) {
+	console.log("a == b is " + (a == b)); 
+    console.log ("a !=b is " + (a != b));
+    console.log ("a > b is " + (a > b));
+    console.log ("a < b is " + (a < b));
+    console.log ("a >= b is " + (a >= b));
+    console.log ("a <= b is " + (a <= b));
+}
+run(10,12);
